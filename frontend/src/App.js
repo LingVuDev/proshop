e9fa14a51272b4e26034292655ee536ac248e16a
Ling Vu <lingvu@Lings-Air.fritz.box>
2020-12-26 14:27:23 +0100

    Setup structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer'
+import Header from './components/Header'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header></Header>
+        <main className="py-3">
+          <Container>
+            <h1>Welcome To ProShop</h1>
+          </Container>
+        </main>
+        <Footer></Footer>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
