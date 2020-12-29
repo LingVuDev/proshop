32b029b1819244a05942bc653a050d1c0e26f2e8
Ling Vu <lingvu@Lings-Air.fritz.box>
2020-12-29 15:16:20 +0100

    Add simple single artile view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react'
-import { Container } from 'react-bootstrap';
+import { BrowserRouter, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
 import Footer from './components/Footer'
 import Header from './components/Header'
-import HomeScreen from './screens/HomeScreen';
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 export class App extends Component {
   render() {
     return (
-      <div>
+      <BrowserRouter>
         <Header></Header>
         <main className="py-3">
           <Container>
-            <HomeScreen></HomeScreen>
+            <Route path="/" component={HomeScreen} exact></Route>
+            <Route path="/product/:id" component={ProductScreen}></Route>
           </Container>
         </main>
         <Footer></Footer>
-      </div>
+      </BrowserRouter>
     )
   }
 }
