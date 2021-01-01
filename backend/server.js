b57ee504335278a3d0e7b848dc7c876bd0161037
Ling Vu <vumanhling@icloud.com>
2021-01-01 15:39:38 +0100

    Add mongo db config

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import products from './data/products.js'
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import products from './data/products.js';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -15,10 +18,15 @@ app.get('/api/products', (req, res) => {
 });
 
 app.get('/api/products/:id', (req, res) => {
-  const product = products.find(singleProduct => singleProduct._id === req.params.id)
+  const product = products.find(
+    (singleProduct) => singleProduct._id === req.params.id
+  );
   res.json(product);
 });
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`));
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+);
